a35a8138ed52484cc65ebb4400b333e1ad5390a8
joshraiborde <joshraiborde@gmail.com>
2022-02-23 14:09:51 -0500

    235 Processing Post Requests with Body Parser,in stalled body parser, required body parser

--- calculator.js
+++ calculator.js
@@ -1,12 +1,25 @@
 const express= require('express')
+const bodyParser = require('body-parser');
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 const port = 3000;
 
 app.get('/', (req, res) => {
+
     res.sendFile(__dirname + "/index.html")
     // console.log(__dirname)
+})
+
+app.post("/", (req, res) =>{
+
+    var num1 = Number(req.body.n1)
+    var num2 = Number(req.body.n2)
+
+    var results = num1 + num2;
+
+    res.send("The result of the calculation is: " + results)
 })
 
 app.listen(port, () =>{
