653198ff0c88d00a5b8042459e008d8dfcfcc43c
joshraiborde <joshraiborde@gmail.com>
2022-02-24 11:18:11 -0500

    prettier formatted

--- calculator.js
+++ calculator.js
@@ -1,47 +1,43 @@
-const express= require('express')
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
-// **************************************************
-app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + "/index.html")
-    // console.log(__dirname)
-})
-
-// BMI CALCULATOR **************************************************
-
-app.get('/bmiCalculator', (req, res) => {
-    res.sendFile(__dirname + "/bmiCalculator.html")
-})
 
 // CALCULATOR **************************************************
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  // console.log(__dirname)
+});
 
-app.post("/", (req, res) =>{
+app.post("/", (req, res) => {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
 
-    var num1 = Number(req.body.n1)
-    var num2 = Number(req.body.n2)
+  var results = num1 + num2;
 
-    var results = num1 + num2;
-
-    res.send("The result of the calculation is: " + results)
-})
+  res.send("The result of the calculation is: " + results);
+});
 
 // BMI CALCULATOR **************************************************
 
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/bmiCalculator", (req, res) => {
-    var w = parseFloat(req.body.w)
-    var h = parseFloat(req.body.h)
+  var w = parseFloat(req.body.w);
+  var h = parseFloat(req.body.h);
 
-    var bmi = Math.round(w/(h*h))
-    // bmi = w*h;
+  var bmi = Math.round(w / (h * h));
 
-    res.send("Your BMI is: " + bmi)
-} )
+  res.send("Your BMI is: " + bmi);
+});
 
-app.listen(port, () =>{
-    console.log("calculator challenge")
-})
+// APP LISTEN **************************************************
+
+app.listen(port, () => {
+  console.log("calculator challenge");
+});
