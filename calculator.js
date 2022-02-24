fbba6347776fa0583b73644d03f432090c2eacb9
joshraiborde <joshraiborde@gmail.com>
2022-02-24 11:16:00 -0500

    236 BMI Routing Challenge

--- calculator.js
+++ calculator.js
@@ -5,12 +5,20 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
 const port = 3000;
-
+// **************************************************
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + "/index.html")
     // console.log(__dirname)
 })
+
+// BMI CALCULATOR **************************************************
+
+app.get('/bmiCalculator', (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+// CALCULATOR **************************************************
 
 app.post("/", (req, res) =>{
 
@@ -21,6 +29,18 @@ app.post("/", (req, res) =>{
 
     res.send("The result of the calculation is: " + results)
 })
+
+// BMI CALCULATOR **************************************************
+
+app.post("/bmiCalculator", (req, res) => {
+    var w = parseFloat(req.body.w)
+    var h = parseFloat(req.body.h)
+
+    var bmi = Math.round(w/(h*h))
+    // bmi = w*h;
+
+    res.send("Your BMI is: " + bmi)
+} )
 
 app.listen(port, () =>{
     console.log("calculator challenge")
