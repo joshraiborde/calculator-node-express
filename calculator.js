29fcb395c867feb299b0f9711168aae641446d91
joshraiborde <joshraiborde@gmail.com>
2022-02-28 17:48:52 -0500

    figuring out .evn and .gitignore

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { get } = require("express/lib/response");
+const res = require("express/lib/response");
+const keys = require("./weatherpai.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
+// const port = 3000;
+
 
 // CALCULATOR **************************************************
 app.get("/", (req, res) => {
@@ -36,8 +39,24 @@ app.post("/bmiCalculator", (req, res) => {
   res.send("Your BMI is: " + bmi);
 });
 
+// weatherpai **************************************************
+
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", (req, res) => {
+  var w = parseFloat(req.body.w);
+  var h = parseFloat(req.body.h);
+
+  var bmi = Math.round(w / (h * h));
+
+  res.send("Your BMI is: " + bmi);
+});
+
+
 // APP LISTEN **************************************************
 
-app.listen(port, () => {
+app.listen(weatherpai.ports.port, () => {
   console.log("calculator challenge");
 });
